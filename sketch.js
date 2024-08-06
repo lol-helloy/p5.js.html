4eb4d11ab68f0aa780228d2362e13484ba40d662
lol-helloy <00001136042015sp@al.educacao.sp.gov.br>
2024-08-06 20:23:32 -0300

    adicionando arquirvos js e css

---
+++ sketch.js
@@ -0,0 +1,34 @@
+let palavra;
+
+function setup() {
+  createCanvas(400, 400);
+
+  palavra = palavraAleatoria();
+}
+
+function palavraAleatoria() {
+  let palavras = ["vo", "nada", "hAHAHA"];
+  return random(palavras);
+}
+
+function inicializaCores() {
+  background("rgb(240,223,4)");
+  fill("rgb(179,146,12)");
+  textSize(64);
+  textAlign(CENTER, CENTER);
+}
+
+function draw() {
+  inicializaCores();
+
+  let maximo = width;
+  let minimo = 0;
+  // mouseX, 0, width ==> 0, palavra.length
+
+  let quantidade = map(mouseX, 0, width, 1, palavra.length);
+  //console.log(quantidade);
+  let parcial = palavra.substring(0, quantidade);
+  text(parcial, 200, 200);
+}
+
+
